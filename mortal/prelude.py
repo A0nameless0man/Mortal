42e4e632beecac5a5b950a2f827b42e78043442d
huguang@WSL.HG-Desktop <1395943920@qq.com>
2022-08-09 02:27:19 +0800

    log to file

--- mortal/prelude.py
+++ mortal/prelude.py
@@ -7,9 +7,9 @@ import numpy as np
 sys.stdin.reconfigure(encoding="utf-8")
 
 logging.basicConfig(
-    stream=sys.stderr,
     level=logging.INFO,
     format="%(asctime)s %(levelname)8s %(filename)12s:%(lineno)-4s %(message)s",
+    handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler(sys.stdout)],
 )
 
 warnings.simplefilter("ignore")
