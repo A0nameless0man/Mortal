821fa5a38ce6ad0dc9c1ad334f4034e569980abb
huguang@WSL.HG-Desktop <1395943920@qq.com>
2022-08-11 23:01:19 +0800

    Fix train play

--- mortal/train_play.py
+++ mortal/train_play.py
@@ -13,9 +13,14 @@ from common import send_msg, recv_msg
 from config import config
 
 
-def train_play(oracle: Brain, mortal: Brain, dqn: DQN):
+def train_play(state):
     device = torch.device(config["control"]["device"])
-
+    oracle = Brain(True, **config["resnet"]).to(device).eval()
+    mortal = Brain(False, **config["resnet"]).to(device).eval()
+    dqn = DQN().to(device)
+    oracle.load_state_dict(state["current_oracle"])
+    mortal.load_state_dict(state["mortal"])
+    dqn.load_state_dict(state["current_dqn"])
     train_player = TrainPlayer()
     rankings, file_list = train_player.train_play(oracle, mortal, dqn, device)
     avg_rank = (rankings * np.arange(1, 5)).sum() / rankings.sum()
