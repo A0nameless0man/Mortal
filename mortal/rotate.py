647afbc4a23cc454357b28cfa17561a9304dba0e
huguang@WSL.HG-Desktop <1395943920@qq.com>
2022-08-12 00:18:20 +0800

    add rotate

---
+++ mortal/rotate.py
@@ -0,0 +1,26 @@
+import prelude
+
+from datetime import datetime
+import logging
+from time import sleep
+
+
+def cp(source, target):
+    with open(source, "rb") as source_file, open(target, "wb") as tar_file:
+        logging.info("{} -> {}".format(source, target))
+        tar_file.write(source_file.read())
+
+
+def rotate():
+    curent = "data/mortal.pth"
+    snapshot = "data/snap.pth"
+    yesterday = "data/yes.pth"
+    archive = "data/" + datetime.now().strftime("%Y%m%d") + ".pth"
+    cp(snapshot,yesterday)
+    cp(curent,snapshot)
+    cp(curent,archive)
+
+def main():
+    while True:
+        rotate()
+        sleep(60*60*24)
